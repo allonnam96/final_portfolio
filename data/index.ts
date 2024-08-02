a7bce84c4920cc67a92d155f05e55bf721690e0f
Allon Nam <allon.nam96@gmail.com>
2024-08-02 16:42:15 -0400

    first commit

---
+++ data/index.ts
@@ -0,0 +1,218 @@
+export const navItems = [
+    { name: "ABOUT", link: "#about" },
+    { name: "PROJECT", link: "#projects" },
+    { name: "TESTIMONIAL", link: "#testimonials" },
+    { name: "CONTACT", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I leverage data to find creative solutions",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Make a meaningful impact on businesses and help others.",
+      description: "",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Ubliss, Medical Aesthetics",
+      des: "At Ubliss, we integrate the latest advancements in aesthetic and functional medicine to offer comprehensive care that enhances your natural beauty while promoting overall well-being.",
+      img: "/Ubliss.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+      link: "https://www.ublissny.com/",
+    },
+    {
+      id: 2,
+      title: "HomeKeeper",
+      des: "Led the Fellow Software Engineers to bring ideas to concrete applications.",
+      img: "/homekeeper.png",
+      iconLists: ["/re.svg", "/git.svg", "/ts.svg",],
+      link: "https://home-keeper.com/",
+    },
+    {
+      id: 3,
+      title: "Twitch",
+      des: "Remaking a pixel-perfect streaming & messaging application.",
+      img: "/twitch.jpg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/allonnam96/nwitch",
+    },
+    {
+      id: 4,
+      title: "Covid Data Visualization",
+      des: "Analyzed statistical data extracted from global COVID-19 infection rates, leveraging the Aggregate functions.",
+      img: "/covid.png",
+      iconLists: ["/sql.png", "/tableau.svg"],
+      link: "https://public.tableau.com/app/profile/hyun.jun.nam/viz/CovidDashboard_17114054591590/Dashboard1",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Allon&apos;s proactive and collaborative approach was instrumental in driving our projects forward. His tireless work ethic and dedication to excellence consistently set a high standard for the team. As a natural communicator, Allon ensured that everyone was aligned and informed, fostering a transparent and efficient workflow. His leadership in managing projects was exemplary, skillfully guiding the team through challenges and delivering outstanding results. Working with Allon was a fantastic experience, and I am confident that his hard work, effective communication, and strong leadership will make him a valuable asset in any future role.",
+      name: "Harrison Liang",
+      title: "Software Engineer @LinkedIn",
+    },
+    {
+      quote:
+        "I had the pleasure of working with Allon as both a Technical Product Manager and Software Engineer. His ability to bridge technical and strategic roles, along with his expertise in software development, made him a key contributor to our team&apos;s success. Allon&apos;s problem-solving skills and collaborative approach were invaluable, and working with him was a fantastic experience. I&apos;m confident he will excel in any future role.",
+      name: "Joseph Yang",
+      title: "Software Engineer @Bloomberg",
+    },
+    {
+      quote:
+        "Allon&apos;s skills as both a Technical Product Manager and Software Engineer were invaluable to our team. His ability to seamlessly integrate technical insights with product strategy, coupled with his problem-solving prowess and collaborative spirit, significantly enhanced our projects. Working with Allon was a pleasure, and I am confident he will bring exceptional value to any future role.",
+      name: "Apollos Kim",
+      title: "Sr. Software Engineer @Bloomberg",
+    },
+    {
+      quote:
+        "Working with Allon as a for software product was an outstanding experience. His keen eye for design and deep understanding of user experience principles consistently resulted in intuitive, visually appealing interfaces that elevated our projects. His creativity and attention to detail greatly contributed to our success, and I am confident he will continue to excel in any future design role.",
+      name: "Boheme, Yoon Ju",
+      title: "Sr. Graphic Designer @IBM",
+    },
+    {
+      quote:
+        "Collaborating with Allon was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Allon&apos;s enthusiasm for every facet of development truly stands out. If you&apos;re seeking to elevate your website and elevate your brand, Allon is the ideal partner.",
+      name: "Stella, RN, BSN",
+      title: "Founder of Ubliss",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Technical Product Manager",
+      desc: "Spearheaded Product Development for 300+ Small Healthcare Business Clients.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+
+    },
+    {
+      id: 2,
+      title: "Software Engineer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+
+    },
+    {
+      id: 3,
+      title: "HeadStarter AI Fellow",
+      desc: "Track: Final project to get 1000 people on waitlist, 1000 accounts created, or $1000 in revenue.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+
+    },
+    {
+      id: 4,
+      title: "Data/Business Analyst",
+      desc: "Seasoned analyst with a background in supply chain technology and a world-renowned cosmetics company.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/allonnam96"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link:"https://www.linkedin.com/in/allonnam/"
+    },
+  ];
